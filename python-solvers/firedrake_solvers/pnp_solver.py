313a077fd0a90e4de3a9be288fae7f2dfa745011
Banman03 <mckeownbaenan123@gmail.com>
2025-11-20 13:15:19 -0600

    working forward problem

--- python-solvers/firedrake_solvers/pnp_solver.py
+++ python-solvers/firedrake_solvers/pnp_solver.py
@@ -1,9 +1,7 @@
-# Firedrake skeleton (tested for structure; adapt parameters & mesh)
 from firedrake import *
 
-# --- parameters (set these to your problem values)
 n_species = n = 2
-order = 1                          # CG1 for concentrations & potential
+order = 1
 dt = 1e-3
 F = 96485.3329
 R = 8.314462618
@@ -28,14 +26,18 @@ electrode_marker = 1
 # --- function spaces: mixed for c_1,...,c_n, phi
 V_scalar = FunctionSpace(mesh, "CG", order)
 mixed_spaces = [V_scalar for _ in range(n)] + [V_scalar]  # last is phi
-W = MixedFunctionSpace(*mixed_spaces)
+W = MixedFunctionSpace(mixed_spaces)
 
 # --- trial / test / function
-U = Function(W)          # current (nonlinear) unknown vector
-U_prev = Function(W)     # previous time step
+U = Function(W)
+U_prev = Function(W)
+
+splitCur = split(U)
+splitPrev = split(U_prev)
+
 v_tests = TestFunctions(W)
 # Break out components
-ci = split(U)[:-1]       # list: c_1,...,c_n (length n)
+ci = split(U)[:-1]
 phi = split(U)[-1]
 ci_prev = split(U_prev)[:-1]
 phi_prev = split(U_prev)[-1]
@@ -48,7 +50,7 @@ w = v_tests[-1]
 kB = Constant(1.380649e-23)  # if you really want k_B; often k_BT used -- adapt units
 kBT = Constant(R*T)          # using R*T is common here (paper uses k_B T or R T depending)
 sum_a_c = sum( Constant(a_vals[i]) * ci[i] for i in range(n) )
-mu_steric = kBT * ln(1 - sum_a_c)   # if a_vals are zero, this term vanishes
+# mu_steric = kBT * ln(1 - sum_a_c)   # if a_vals are zero, this term vanishes
 # grad(mu_steric) computed by UFL via grad(mu_steric)
 
 # --- variational residual
@@ -63,7 +65,7 @@ for i in range(n):
     F_res += ( (c - c_old)/dt * v )*dx
 
     # diffusion + drift (steric included)
-    drift_potential = F_over_RT * z * phi + mu_steric
+    drift_potential = F_over_RT * z * phi # + mu_steric
     Jflux = D*(grad(c) + c * grad(drift_potential))
     F_res += dot(Jflux, grad(v))*dx
 
@@ -72,7 +74,7 @@ for i in range(n):
     eta = phi - phi_eq
     j = j0*( exp(-alpha*n_electrons*F_over_RT*eta) - exp((1-alpha)*n_electrons*F_over_RT*eta) )
     # sign: choose + or - per reaction; here we add the contribution (paper uses +/-)
-    F_res -= ( (+1.0/(n_electrons*F)) * j * v )*ds(electrode_marker)
+    # F_res -= ( (+1.0/(n_electrons*F)) * j * v )*ds(electrode_marker)
 
 # Poisson residual
 eps = Constant(1.0)  # permittivity (set appropriately)
@@ -81,25 +83,64 @@ F_res += eps*dot(grad(phi), grad(phi_test))*dx
 F_res -= sum( Constant(z_vals[i])*F * ci[i]*phi_test for i in range(n) )*dx
 # add Neumann bc on phi if necessary: subtract flux * w over Gamma_N
 
+
+phi0 = 0.0
+c0 = 1.0
+
+bc_phi = DirichletBC(W.sub(n), Constant(phi0), 1)
+bc_ci  = [DirichletBC(W.sub(i), Constant(c0), 3) for i in range(n)]
+
+bcs = bc_ci + [bc_phi]
+
 # --- form the Jacobian automatically
 J = derivative(F_res, U)
 
 # --- initial condition: fill U_prev with initial c's and phi (e.g. uniform)
-assign(U_prev.sub(0), interpolate(Constant(1.0), V_scalar))  # etc.
+for i in range(n):
+    U_prev.sub(i).assign(Constant(c0))
+U_prev.sub(n).assign(Constant(phi0))
 
 # --- solve monolithically using NonlinearVariationalSolver
-problem = NonlinearVariationalProblem(F_res, U, bcs=None, J=J)
+problem = NonlinearVariationalProblem(F_res, U, bcs=bcs, J=J)
 solver_params = {
-    'snes_type': 'newtonls',  # Newton line-search
+    'snes_type': 'newtonls',
     'snes_max_it': 50,
     'snes_rtol': 1e-9,
     'ksp_type': 'preonly',
     # use field split (block) preconditioner: separate species block(s) and potential block
     'pc_type': 'fieldsplit',
-    'pc_fieldsplit_type': 'schur',
-    'pc_fieldsplit_schur_fact_type': 'FULL',
+    'pc_fieldsplit_type': 'additive',
     # setup fieldsplit names / splits afterwards in options context if needed
 }
 
 solver = NonlinearVariationalSolver(problem, solver_parameters=solver_params)
 solver.solve()
+
+components = split(U)
+# concentrations
+ci = components[:-1]
+# potential
+phi = components[-1]
+
+phi_file = VTKFile("phi.pvd")
+phi_file.write(phi)
+
+for i, c in enumerate(ci):
+    file = VTKFile(f"c{i}.pvd")
+    file.write(c)
+    
+    
+    
+'''
+Plot something as a magnitude, and, because it's a vector field, we can plot arrows going in different directions
+You can also plot the potential on top of the concentrations
+
+We can also look at different snapshots of the solution to see if its converging or not
+We can also look at a time-dependent problem if the above doesn't work
+
+To check convergence: Use L2 norm of true and FEM solution.
+We can also use the H1 (tracks the error in the first derivative of the actual solution and FEM solution)
+We can also use L2 trace and H1 trace (what is the error happening at the boundary) errors.
+Refinement of the mesh (see if the error decreases as the grid is refined).
+Sometimes if you use quadratic polynomials instead of piecewise linear can improve convergence.
+'''
